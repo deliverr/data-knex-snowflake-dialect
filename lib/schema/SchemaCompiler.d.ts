8df5ffecde330dd455c187dcf4216502eeeb3661
Udit Kapoor <udit.kapoor@deliverr.com>
2022-06-06 10:00:26 -0400

    remove lib

--- lib/schema/SchemaCompiler.d.ts
+++
@@ -1,6 +0,0 @@
-// @ts-ignore
-import * as SchemaCompiler_MySQL from "knex/lib/dialects/mysql/schema/compiler";
-export declare class SchemaCompiler extends SchemaCompiler_MySQL {
-    constructor(client: any, builder: any);
-    hasTable(tableName: string): void;
-}
