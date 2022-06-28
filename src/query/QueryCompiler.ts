6aa80141e876d5d08d27fd963c83e11574d8ca55
Hans Thielen <hans.thielen@inform-software.com>
2022-06-28 15:35:36 +0200

    Fixes schema handling for hasTable and columnInfo

--- src/query/QueryCompiler.ts
+++ src/query/QueryCompiler.ts
@@ -20,4 +20,45 @@ export class QueryCompiler extends QueryCompiler_MySQL {
     return '';
   }
 
+  columnInfo() {
+    // @ts-ignore
+    const column = this.single.columnInfo;
+    // @ts-ignore
+    const tableName = (this.single.table as string).toUpperCase();
+    const [ schemaFromTable, table ] = tableName.includes(".") ? tableName.split(".") : [undefined, tableName];
+    let sql = 'select * from information_schema.columns where table_name = ?';
+    const bindings = [table.toUpperCase()];
+    let schema;
+    if (schemaFromTable) {
+      schema = schemaFromTable.toUpperCase();
+      sql += ' and table_schema = ?';
+      bindings.push(schema);
+      // @ts-ignore
+    } else if (this.client.connectionSettings.schema) {
+      // @ts-ignore
+      schema = this.client.connectionSettings.schema.toUpperCase();
+      sql += ' and table_schema = ?';
+      bindings.push(schema);
+    } else {
+      sql += ' and table_schema = current_schema()';
+    }
+    return {
+      sql,
+      // @ts-ignore
+      bindings,
+      output(resp) {
+        const out = resp.rows.reduce((columns, val) => {
+          columns[val.COLUMN_NAME] = {
+            defaultValue: val.COLUMN_DEFAULT === 'NULL' ? null : val.COLUMN_DEFAULT,
+            type: val.DATA_TYPE,
+            maxLength: val.CHARACTER_MAXIMUM_LENGTH,
+            nullable: val.IS_NULLABLE === 'YES',
+          };
+          return columns;
+        }, {});
+        return (column && out[column]) || out;
+      },
+    };
+  }
+
 }
